09d0ec58211d65ed0d8b5e4004f2e440bf95cd10
angcyo <angcyo@126.com>
2025-01-16 23:25:15 +0800

    * fix udp abc

--- flutter3_abc/lib/src/udp_service_abc.dart
+++ flutter3_abc/lib/src/udp_service_abc.dart
@@ -13,28 +13,110 @@ class UdpServiceAbc extends StatefulWidget {
 }
 
 class _UdpServiceAbcState extends State<UdpServiceAbc> with BaseAbcStateMixin {
+  final shelf.DefaultUdpService defaultUdpService =
+      shelf.DefaultUdpService.instance;
+  final shelf.UdpService udpService = shelf.DefaultUdpService.instance;
+
+  /// 选中的设备id
+  String? _selectedDeviceId;
+
   @override
-  WidgetList buildBodyList(BuildContext context) {
+  void dispose() {
+    if (udpService != shelf.DefaultUdpService.instance) {
+      udpService.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget buildAbc(BuildContext context) {
+    final globalTheme = GlobalTheme.of(context);
     return [
       [
         GradientButton.normal(() async {
-          shelf.DefaultUdpService.instance.startServer();
+          udpService.startServer();
           updateState();
         }, child: "启动服务端".text()),
         GradientButton.normal(() {
-          shelf.DefaultUdpService.instance.stopServer();
+          udpService.stopServer();
         }, child: "停止服务端".text()),
         GradientButton.normal(() async {
-          shelf.DefaultUdpService.instance.startClient();
+          udpService.startClient();
           updateState();
         }, child: "启动客户端".text()),
         GradientButton.normal(() {
-          shelf.DefaultUdpService.instance.stopClient();
+          udpService.stopClient();
         }, child: "停止客户端".text()),
         GradientButton.normal(() {
-          shelf.DefaultUdpService.instance.sendBroadcast(nowTimeString());
+          udpService.sendBroadcast(nowTimeString().bytes);
         }, child: "发送广播".text()),
-      ].flowLayout(childGap: kX, padding: const EdgeInsets.all(kX))!,
+      ]
+          .flowLayout(childGap: kX, padding: const EdgeInsets.all(kX))!
+          .matchParentWidth(),
+      [defaultUdpService.serverInfoSignal, defaultUdpService.clientInfoSignal]
+          .buildFn(() {
+        final serverInfo = defaultUdpService.serverInfoSignal.value;
+        final clientInfo = defaultUdpService.clientInfoSignal.value;
+        if (serverInfo == null && clientInfo == null) {
+          return empty;
+        }
+        return textSpanBuilder((builder) {
+          if (serverInfo != null) {
+            builder.addText("服务端已启动: ");
+            builder.addText(serverInfo.toString());
+          }
+          if (clientInfo != null) {
+            builder.addText("客户端已启动: ");
+            builder.addText(clientInfo.toString());
+          }
+        });
+      }),
+      defaultUdpService.clientListStream.buildFn(() {
+        final clientList = defaultUdpService.getServerClientList();
+        if (isNil(clientList)) {
+          return empty;
+        }
+        return [
+          for (final clientInfo in clientList)
+            textSpanBuilder((builder) {
+              builder.addText("客户端(${clientInfo.time?.toTimeString()}):\n",
+                  style: globalTheme.textDesStyle);
+              builder.addText(clientInfo.clientShowName,
+                  style: globalTheme.textBodyStyle);
+            })
+                .align(Alignment.centerLeft)
+                .paddingAll(kH)
+                .backgroundColor(_selectedDeviceId == clientInfo.deviceId
+                    ? globalTheme.pressColor
+                    : null)
+                .ink(
+              () {
+                _selectedDeviceId = clientInfo.deviceId;
+                updateState();
+              },
+              enable: _selectedDeviceId != clientInfo.deviceId,
+            ).matchParentHeight(),
+        ].scroll()?.size(height: 100, width: double.infinity);
+      }),
+      super.buildAbc(context).expanded(),
+    ].column()!;
+  }
+
+  @override
+  WidgetList buildBodyList(BuildContext context) {
+    return [
+      if (_selectedDeviceId == null) "!请先选择客户端!".text().center().sliverExpand(),
+      if (_selectedDeviceId != null)
+        ...() {
+          final messageList =
+              defaultUdpService.getServerClientMessageList(_selectedDeviceId);
+          if (isNil(messageList)) {
+            return ["等待客户端消息...".text().center().sliverExpand()];
+          }
+          return [
+            for (final message in messageList) "$message".text(),
+          ];
+        }(),
     ];
   }
 }
