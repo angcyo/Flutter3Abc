9a3e6d4a4b61529444a92f6728756d1d41177909
angcyo <angcyo@126.com>
2025-02-18 22:33:24 +0800

    * fix abc

--- flutter3_abc/lib/src/path_abc.dart
+++ flutter3_abc/lib/src/path_abc.dart
@@ -16,26 +16,38 @@ class _PathAbcState extends State<PathAbc> with AbsScrollPage {
 
   @override
   WidgetList? buildScrollBody(BuildContext context) {
-    final path = Path();
-    path.moveTo(10, 10);
-    path.lineTo(100, 20);
+    final p1 =
+        "m23.167,330.833v-7h4.667v7h5.833v-9.333h3.5l-11.667-10.5-11.667,10.5h3.5v9.333h5.833Z"
+            .toUiPath();
+
+    final p2 = Path();
+    p2.moveTo(10, 10);
+    p2.lineTo(100, 20);
     return [
       PathWidget(
-        path:
-            "m23.167,330.833v-7h4.667v7h5.833v-9.333h3.5l-11.667-10.5-11.667,10.5h3.5v9.333h5.833Z"
-                .toUiPath(),
+        path: p1,
+        shader: sweepGradientShader(
+          [Colors.blueAccent, Colors.redAccent],
+          rect: p1.getBounds(),
+        ),
       ).bounds(),
       HttpDeviceXyzControlWidget().bounds(),
       //--
       Divider(),
-      PathWidget(path: path).bounds(),
+      PathWidget(
+        path: p2,
+        shader: linearGradientShader(
+          [Colors.blueAccent, Colors.redAccent],
+          rect: p2.getBounds(),
+        ),
+      ).bounds(),
       if (!isNil(svgPath)) svgPath!.text(),
       [
         GradientButton.min(
           child: "each path".text(),
           onTap: () {
             svgPath = stringBuilder((b) {
-              path.eachPathMetrics(
+              p2.eachPathMetrics(
                   (posIndex, ratio, contourIndex, position, angle, isClose) {
                 b.append(
                     "[$contourIndex/$posIndex] position:${position.log}, angle:$angle, isClose:$isClose $ratio\n");
@@ -48,7 +60,7 @@ class _PathAbcState extends State<PathAbc> with AbsScrollPage {
           child: "each path(async)".text(),
           onTap: () async {
             StringBuffer buffer = StringBuffer();
-            await path.eachPathMetricsAsync(
+            await p2.eachPathMetricsAsync(
                 (posIndex, ratio, contourIndex, position, angle, isClose) {
               buffer.write(
                   "[$contourIndex/$posIndex] position:${position.log}, angle:$angle, isClose:$isClose $ratio\n");
@@ -60,7 +72,7 @@ class _PathAbcState extends State<PathAbc> with AbsScrollPage {
         GradientButton.min(
           child: "path to svg(async)".text(),
           onTap: () async {
-            svgPath = await path.toSvgPathStringAsync(
+            svgPath = await p2.toSvgPathStringAsync(
               pathStep: kPathAcceptableError,
               tolerance: $lpDeviceKeys.vectorTolerance,
               contourInterval: 1,
@@ -73,7 +85,7 @@ class _PathAbcState extends State<PathAbc> with AbsScrollPage {
         GradientButton.min(
           child: "path to svg(async)2".text(),
           onTap: () async {
-            svgPath = await path.toSvgPathStringAsync(
+            svgPath = await p2.toSvgPathStringAsync(
               contourInterval: 1,
               stepInterval: null,
               digits: 3,
@@ -84,7 +96,7 @@ class _PathAbcState extends State<PathAbc> with AbsScrollPage {
         GradientButton.min(
           child: "path to svg(async)3".text(),
           onTap: () async {
-            svgPath = await path.toSvgPathStringAsync(
+            svgPath = await p2.toSvgPathStringAsync(
               contourInterval: null,
               stepInterval: null,
               digits: 3,
@@ -95,7 +107,7 @@ class _PathAbcState extends State<PathAbc> with AbsScrollPage {
         GradientButton.min(
           child: "path to svg".text(),
           onTap: () {
-            svgPath = path.toSvgPathString(
+            svgPath = p2.toSvgPathString(
               pathStep: kPathAcceptableError,
               tolerance: $lpDeviceKeys.vectorTolerance,
               digits: 3,
