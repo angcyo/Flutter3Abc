ded7c6c6d110c429a861f5d090c8a1f1abc2271c
angcyo <angcyo@126.com>
2025-07-04 09:25:03 +0800

    + socket abc

--- flutter3_abc/lib/src/socket_abc.dart
+++ flutter3_abc/lib/src/socket_abc.dart
@@ -1,9 +1,7 @@
-import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter3_app/flutter3_app.dart';
-import 'package:flutter3_basics/flutter3_basics.dart';
 
 import '../flutter3_abc.dart';
 
@@ -83,6 +81,7 @@ class _SocketAbcState extends State<SocketAbc> with BaseAbcStateMixin {
         IconButton(
           icon: Icon(Icons.info_outline),
           onPressed: () {
+            l.d(networkInterfaceSignal.value);
             toastInfo(networkInterfaceSignal.value);
           },
         ).hoverTooltip(networkInterfaceSignal.value
@@ -136,12 +135,37 @@ class _SocketAbcState extends State<SocketAbc> with BaseAbcStateMixin {
   /// 网口信息
   final networkInterfaceSignal = $signal<String>();
 
+  /// - IPV4环回地址: 127.0.0.1
+  /// - IPV6环回地址: ::1
+  ///
+  /// - lo0（Loopback Interface）: 本地环回地址, 通常被称为 "localhost" 或 "loopback"。
+  /// - utun0（VPN Interface）: utun 是用于虚拟隧道（如 VPN）的网络接口。
+  /// - en0（Ethernet Interface）: en0 代表系统中的第一个以太网接口，它通常是物理网络接口例如 Ethernet 端口或 Wi-Fi 适配器。
+  /// - awdl0: awdl 代表 Apple Wireless Direct Link，用于与其他 Apple 设备（如 iPhone、iPad、Mac）进行点对点的无线通信。
+  /// - llw0: llw0 代表 Apple Link-Local Wireless，低延迟无线通信. 用于 Apple 设备之间的无线通信。
+  ///
+  /// # MacOS
+  /// ```
+  /// NetworkInterface('lo0', [InternetAddress('127.0.0.1', IPv4), InternetAddress('::1', IPv6), InternetAddress('fe80::1%lo0', IPv6)])↓
+  /// NetworkInterface('en0', [InternetAddress('192.168.31.232', IPv4)])↓
+  /// NetworkInterface('en8', [InternetAddress('fe80::fcda:1cff:feb4:51b8%en8', IPv6)])↓
+  /// NetworkInterface('en12', [InternetAddress('fe80::18c1:53c6:8e6e:8baf%en12', IPv6), InternetAddress('169.254.30.229', IPv4)])↓
+  /// NetworkInterface('utun0', [InternetAddress('fe80::d04e:ca70:43a1:d9c0%utun0', IPv6)])↓
+  /// NetworkInterface('utun1', [InternetAddress('fe80::5950:6fce:5c3:8129%utun1', IPv6)])↓
+  /// NetworkInterface('utun2', [InternetAddress('fe80::4f79:5787:2f4f:d0bb%utun2', IPv6)])↓
+  /// NetworkInterface('utun3', [InternetAddress('fe80::ce81:b1c:bd2c:69e%utun3', IPv6)])↓
+  /// NetworkInterface('utun5', [InternetAddress('fe80::a08:b58a:4b96:d1ba%utun5', IPv6)])↓
+  /// NetworkInterface('utun6', [InternetAddress('fe80::691b:9545:fd59:be96%utun6', IPv6)])
+  /// NetworkInterface('awdl0', [InternetAddress('fe80::684a:a1ff:fe36:f613%awdl0', IPv6)])↓
+  /// NetworkInterface('llw0', [InternetAddress('fe80::684a:a1ff:fe36:f613%llw0', IPv6)])↓
+  /// ```
   void _loadNetworkInterface() async {
     final list = await NetworkInterface.list(
       includeLinkLocal: true,
       type: InternetAddressType.any,
       includeLoopback: true,
     );
+    //debugger();
     final ipv4 = InternetAddress.anyIPv4.address; //0.0.0.0
     final ipv6 = InternetAddress.anyIPv6.address; //::
     networkInterfaceSignal.updateValue(
