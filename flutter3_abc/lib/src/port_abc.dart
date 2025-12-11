9696c871f0c2c8eea9f8a7a402b8c93738019ca3
angcyo <angcyo@126.com>
2025-12-11 19:27:14 +0800

    * fix port abc

--- flutter3_abc/lib/src/port_abc.dart
+++ flutter3_abc/lib/src/port_abc.dart
@@ -12,11 +12,97 @@ class PortAbc extends StatefulWidget {
   State<PortAbc> createState() => _PortAbcState();
 }
 
-class _PortAbcState extends State<PortAbc> {
+class _PortAbcState extends State<PortAbc> with BaseAbcStateMixin {
+  @output
+  String? _localIp;
+
+  @output
+  bool _isScanning = false;
+
   @override
-  Widget build(BuildContext context) {
-    return $discoverDeviceIp().toWidget((ctx, data) {
-      return "$data".text();
+  void initState() {
+    $discoverDeviceIp().get((data, error) {
+      _localIp = data;
+      updateState();
     });
+    super.initState();
+  }
+
+  @override
+  Widget buildAbc(BuildContext context) {
+    final globalTheme = GlobalTheme.of(context);
+    return [
+      if (_isScanning)
+        RadarScanWidget(
+          radarColor: globalTheme.accentColor.withOpacity(0.5),
+          radarScanColor: globalTheme.accentColor.withOpacity(0.5),
+        ),
+      super.buildAbc(context),
+    ].stack()!;
+  }
+
+  @override
+  WidgetList buildBodyList(BuildContext context) {
+    return [
+      [
+        "当前本地ip: $_localIp".text().insets(all: kX),
+        GradientButton.normal(() {
+          _isScanning = !_isScanning;
+          if (_isScanning) {
+            startScanPort();
+          }
+          updateState();
+        }, child: (_isScanning ? "停止扫描端口" : "开始扫描端口").text()),
+        SingleInputWidget(
+          labelText: "需要扫描的端口",
+          config: scanPortField,
+        ).size(width: 220).insets(horizontal: kX, vertical: kX),
+      ].flowLayout()!.insets(horizontal: kX),
+      for (final item in _addressList)
+        "[${item.openPorts.first}]${item.ip}".text().insets(
+          horizontal: kX,
+          vertical: kL,
+        ),
+    ];
+  }
+
+  /// 需要扫描的端口
+  late TextFieldConfig scanPortField = TextFieldConfig(
+    text: "scanPortField".hiveGet("80"),
+    hintText: "需要扫描的端口",
+    notifyDefaultTextChange: true,
+    onChanged: (value) {
+      //debugger();
+      "scanPortField".hivePut(value.isEmpty ? null : value);
+      //blueDevice.scanFilterKeywords = value.splitAndTrim(" ");
+    },
+  );
+
+  //MARK: - api
+
+  /// 查找到的ip地址
+  @output
+  final List<NetworkAddress> _addressList = [];
+
+  /// 开始扫描端口
+  @api
+  void startScanPort() {
+    _addressList.clear();
+    updateState();
+    PortScanner.discover(
+      _localIp!.subnet,
+      scanPortField.text.toIntOrNull() ?? 80,
+      onFindAddress: (address) {
+        _addressList.add(address);
+        updateState();
+        //debugger();
+      },
+    ).listen(
+      (data) {},
+      onDone: () {
+        _isScanning = false;
+        updateState();
+      },
+    );
   }
 }
