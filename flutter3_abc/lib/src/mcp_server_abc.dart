999f2401cdcae7fd982b1011bde8cef3f6459685
angcyo <angcyo@126.com>
2026-01-04 18:15:18 +0800

    + mcp abc

--- flutter3_abc/lib/src/mcp_server_abc.dart
+++ flutter3_abc/lib/src/mcp_server_abc.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter3_abc/flutter3_abc.dart';
-import 'package:flutter3_basics/flutter3_basics.dart';
-import 'package:flutter3_shelf/flutter3_shelf.dart';
+import 'package:flutter3_app/flutter3_app.dart';
+import 'package:flutter3_mcp/flutter3_mcp.dart';
 
 ///
 /// @author <a href="mailto:angcyo@126.com">angcyo</a>
@@ -16,10 +16,13 @@ class MCPServerAbc extends StatefulWidget {
 
 class _MCPServerAbcState extends State<MCPServerAbc>
     with BaseAbcStateMixin, HookMixin, HookStateMixin {
+  /// mcp server
+  StreamableMcpServer? debugMcpServer;
+
   @override
   void initState() {
     super.initState();
-    startWebSocketServer(9999, null, debugLabel: "MCPServerAbc");
+    //startWebSocketServer(9999, null, debugLabel: "MCPServerAbc");
   }
 
   @override
@@ -28,7 +31,30 @@ class _MCPServerAbcState extends State<MCPServerAbc>
   }
 
   @override
+  void dispose() {
+    debugMcpServer?.stop();
+    debugMcpServer = null;
+    super.dispose();
+  }
+
+  @override
   WidgetList buildBodyList(BuildContext context) {
-    return super.buildBodyList(context);
+    return [
+      [
+        GradientButton.normal(() {
+          if (debugMcpServer != null) {
+            debugMcpServer?.stop().get((data, error) {
+              debugMcpServer = null;
+              updateState();
+            });
+          } else {
+            DebugMcpServer.start().get((data, error) {
+              debugMcpServer = data;
+              updateState();
+            });
+          }
+        }, child: (debugMcpServer != null ? "停止mcp服务" : "启动mcp服务").text()),
+      ].flowLayout(childGap: kL)!.insets(all: kL),
+    ];
   }
 }
