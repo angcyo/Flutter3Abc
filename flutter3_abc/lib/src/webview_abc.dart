dbb426d1963bea74af9e7e336bcea76b001d96ee
angcyo <angcyo@126.com>
2026-04-26 09:02:59 +0800

    + abc

--- flutter3_abc/lib/src/webview_abc.dart
+++ flutter3_abc/lib/src/webview_abc.dart
@@ -159,7 +159,7 @@ class _WebviewAbcState extends State<WebviewAbc>
         GradientButton.normal(() {
           urlFieldConfig.text = "http://192.168.1.175:5500/";
           loadWebviewUrl(urlFieldConfig.text);
-        }, child: "->t2".text()),
+        }, child: "->5500".text()),
         GradientButton.normal(() {
           urlFieldConfig.text =
               "http://192.168.1.175:5500/?"
@@ -169,6 +169,10 @@ class _WebviewAbcState extends State<WebviewAbc>
               "&token=eyJraWQiOiI5MDQ1YjgwYi05YmU0LTRiNTgtOTU2Zi1hZjAzYzk4ZDNkMzIiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJ1c2VybmFtZSIsImF1ZCI6Im9hdXRoLWNsaWVudC1pZCIsIm5iZiI6MTc3NjA0ODU3MSwic2hvcnRUb2tlbiI6Imd0Ry91Tkhtc21hV3NoMzgwREp6MGJ4YjhDSEsyM1VzdXBZdXRKL0tXeHFpRTArNnNqTCtQa3hiSDhwSUdRZ2VuWmJvVW9yRFcwZ0RScEhNQVZIbDJBPT0iLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjI5MDAxIiwiZXhwIjoxNzc4NjQwNTcxLCJpYXQiOjE3NzYwNDg1NzEsInVzZXIiOnsidXNlcklkIjoiMTcyMDk0N2ZmYjM5NDBmZjk1NjFkMGRjMzU2NzJhOGEiLCJhY2NvdW50SWQiOiI1NjlkZGU4YTZhNGE5MzQxNzY0OWVjNWY5YjdhNWFmMiIsInVzZXJuYW1lIjoidXNlcm5hbWUiLCJuYW1lIjoidXNlcm5hbWUiLCJjb2RlIjpudWxsLCJzZXgiOm51bGwsIm1vYmlsZSI6bnVsbCwiZW1haWwiOm51bGwsImNhdGVnb3J5IjoxLCJoZWFkSW1nVXJsIjpudWxsLCJzb3VyY2VPZlJlcXVlc3QiOiJBUFAiLCJsb2dpblRpbWUiOiIyMDI2LTA0LTEzIDEwOjQ5OjMxIn0sImp0aSI6ImM3M2UzODdhLTJkMDItNDc1NS1iZTg0LTRkZGNmYTllODVkNyJ9.YcAG4A-tc6B-XMScNKnBhmyJpzlqr9_4MyPEGjotqfSF3sXMV-O4XoEkFprG_pHtcpn7QtKwEF26X4vOqnR7pGl1o5L7DoqifwhyUSjyQneQPFtM8l2HQ_hU0UOUuEHey2IpYKTUS67gC0Dro_lXGu3Ka_-ktYSj2ev5JI0APWO4QDJWo0qHgZYjPlAeGoJGzTDt5HpEHH--o9rq-rWsbu62svM_CxPRvqeDof9wzal6LI8wK0pTqUNwVzQaE2AHkCYUkTAfKlQ_T7Ag-Maj9RzWKhxjf2d3P6IDFwk5bRgZEpRIXih0KQ6pula5XEfoyQEy8zxlpTaEaRZ4E_-aGg&regionId=c89b900a29bdc1672d5e3da25f1d3e6d";
           loadWebviewUrl(urlFieldConfig.text);
         }, child: "->t3".text()),
+        GradientButton.normal(() {
+          urlFieldConfig.text = "http://192.168.31.77:5173/";
+          loadWebviewUrl(urlFieldConfig.text);
+        }, child: "->5173".text()),
       ].flowLayout(
         selfConstraints: const LayoutBoxConstraints(
           widthType: ConstraintsType.matchParent,
