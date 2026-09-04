7c55fb3df438218f3c0c7053b301c58d6e7fb10e
angcyo <angcyo@126.com>
2026-09-04 16:42:28 +0800

    * init

--- flutter3_abc/lib/src/app_update_abc.dart
+++ flutter3_abc/lib/src/app_update_abc.dart
@@ -14,7 +14,8 @@ class AppUpdateAbc extends StatefulWidget {
 class _AppUpdateAbcState extends State<AppUpdateAbc> with BaseAbcStateMixin {
   late final TextFieldConfig _lastAndroidMarketUrlConfig = TextFieldConfig(
     hintText: "Android市场跳转链接",
-    text: "_lastAndroidMarketUrlConfig".hiveGet() ??
+    text:
+        "_lastAndroidMarketUrlConfig".hiveGet() ??
         "market://details?id=com.tencent.mm",
     textInputAction: TextInputAction.done,
     onChanged: (value) {
@@ -24,7 +25,8 @@ class _AppUpdateAbcState extends State<AppUpdateAbc> with BaseAbcStateMixin {
 
   late final TextFieldConfig _lastIosMarketUrlConfig = TextFieldConfig(
     hintText: "iOS市场跳转链接",
-    text: "_lastIosMarketUrlConfig".hiveGet() ??
+    text:
+        "_lastIosMarketUrlConfig".hiveGet() ??
         "itms-apps://itunes.apple.com/app/id6445970960",
     textInputAction: TextInputAction.done,
     onChanged: (value) {
@@ -63,45 +65,49 @@ class _AppUpdateAbcState extends State<AppUpdateAbc> with BaseAbcStateMixin {
       ),
       [
         GradientButton(
-            onTap: () {
-              _lastAndroidMarketUrlConfig.text.launch();
-            },
-            child: "跳转Android市场".text()),
+          onTap: () {
+            _lastAndroidMarketUrlConfig.text.launch();
+          },
+          child: "跳转Android市场".text(),
+        ),
         GradientButton(
-            onTap: () {
-              _lastIosMarketUrlConfig.text.launch();
-            },
-            child: "跳转iOS市场".text()),
+          onTap: () {
+            _lastIosMarketUrlConfig.text.launch();
+          },
+          child: "跳转iOS市场".text(),
+        ),
         GradientButton(
-            onTap: () {
-              AppUpdateDialog.checkUpdateAndShow(
-                context,
-                LibAppVersionBean()
-                  ..versionName = "1.0.1"
-                  ..versionTile = "新版本升级"
-                  ..forceUpdate = _force
-                  ..outLink = false
-                  ..jumpToMarket = _jumpToMarket
-                  //..marketUrl = "itms-apps://itunes.apple.com/app/id6445970960"
-                  ..marketUrl = "market://details?id=com.tencent.mm"
-                  ..downloadUrl =
-                      "https://gitcode.net/angcyo/file/-/raw/master/BackManage/BackManage-1.2.0_pre_pretest_app.apk"
-                  ..versionDes = "1:angcyo\n2:angcyo" * 1,
-                forceShow: isDebug,
-              );
-            },
-            child: "显示更新弹窗".text()),
+          onTap: () {
+            AppUpdateHandler.checkUpdateAndShow(
+              context,
+              LibAppVersionBean()
+                ..versionName = "1.0.1"
+                ..versionTile = "新版本升级"
+                ..forceUpdate = _force
+                ..outLink = false
+                ..jumpToMarket = _jumpToMarket
+                //..marketUrl = "itms-apps://itunes.apple.com/app/id6445970960"
+                ..marketUrl = "market://details?id=com.tencent.mm"
+                ..downloadUrl =
+                    "https://gitcode.net/angcyo/file/-/raw/master/BackManage/BackManage-1.2.0_pre_pretest_app.apk"
+                ..versionDes = "1:angcyo\n2:angcyo" * 1,
+              forceShow: isDebug,
+            );
+          },
+          child: "显示更新弹窗".text(),
+        ),
         GradientButton(
-            onTap: () {
-              AppUpdateDialog.checkUpdateAndShow(
-                context,
-                LibAppVersionBean()
-                  ..forbiddenReason = "forbiddenReason"
-                  ..forceForbidden = _force,
-                forceShow: false,
-              );
-            },
-            child: "forbidden".text()),
+          onTap: () {
+            AppUpdateHandler.checkUpdateAndShow(
+              context,
+              LibAppVersionBean()
+                ..forbiddenReason = "forbiddenReason"
+                ..forceForbidden = _force,
+              forceShow: false,
+            );
+          },
+          child: "forbidden".text(),
+        ),
       ].flowLayout(padding: kXInsets, childGap: kX)!,
     ];
   }
